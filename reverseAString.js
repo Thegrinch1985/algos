7ce1042118d8ea04240db6b55bb0f6f29f4d0c80
Howie Lynch <thegrinch1985@gmail.com>
2022-01-18 15:12:33 +0000

    Create reverseAString.js

---
+++ reverseAString.js
@@ -0,0 +1,11 @@
+// reverse a string without using reverse/split functions//
+
+function reverseAString(str){
+    let newString = "";
+    for(let i = str.length-1; i >= 0; i--){
+      newString += str[i];
+    }
+    return newString;
+  }
+  
+  console.log(reverseAString("javascript"))
